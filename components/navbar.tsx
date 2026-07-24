601f6da495d8716edb47e33b0289421f11d9f5da
Bibek Das <bibekdastm@gmail.com>
2026-07-24 14:05:11 +0530

    responsive fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,10 +6,21 @@ import { Button, Menu } from "@/components";
 
 export default function Navbar() {
 	return (
-		<div className="fixed top-0 left-0 w-full flex justify-between items-center py-8 px-8 z-50">
+		<div
+			className="fixed top-0 left-0 w-full
+			flex flex-col md:flex-row gap-2 md:gap-0
+			justify-between items-center
+			py-8 px-8
+			z-50"
+		>
 
+			{/*LOGO*/}
 			<div className="flex gap-2 self-start">
-				<div className="flex items-start gap-10">
+
+				<div
+					className="flex gap-10
+					lg:scale-100 md:scale-90 scale-80"
+				>
 					<div>
 						<Image
 							src={logo}
@@ -18,9 +29,10 @@ export default function Navbar() {
 							height={180}
 						/>
 					</div>
+
 					<div>
 						<p
-							className="text-sm uppercase text-white font-medium tracking-tight leading-tight font-helveticaNeue">
+							className="text-sm uppercase text-white tracking-tight leading-tight">
 							PART OF THE
 						</p>
 						<Link href="/">
@@ -34,14 +46,15 @@ export default function Navbar() {
 					</div>
 				</div>
 
-
-
-
 			</div>
+
+
 			{/*<div className="self-start mt-9">*/}
 			{/*	<Menu />*/}
 			{/*</div>*/}
-			<div className="flex items-center gap-2 self-start">
+
+
+			<div className="hidden md:flex flex-col lg:flex-row items-center gap-2 lg:self-start ">
 				<Button
 					title="BECOME A SPEAKER"
 					textColor="#150c1c"
